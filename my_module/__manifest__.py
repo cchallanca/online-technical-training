02f22ab1b10dfa73df16b62d5f6862b49529f6b7
Carlos <odoo@ampd02.odoo.com>
2022-09-21 14:44:13 +0000

    [ADD] new module

---
+++ my_module/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mi módulo',
+    'summary': """Aplicación nueva creada recientemente""",
+    'description': """
+        Aplicación con 
+        - Submódulo 1
+        - Submódulo 2
+        - Submódulo 3
+    """,
+    'author': 'Carlos Challanca',
+    'website': 'https://facebook.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
